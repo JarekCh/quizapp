54aa20e35a6dabd3ceca5ffb83c15a6e4126468e
Jaroslaw Charchula <charchula@gmail.com>
2022-06-30 12:23:35 +0200

    set partial file structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-
-import './App.css';
+import './styles.css';
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <main className="App">
+      {/* question comp os starting screen */}
+      <div className='btn__container'>
+        {/* contidional score rendering */}
+        {/* btn with conditional rendering text check answers -> play again */}
+      </div>
+    </main>
   );
 }
 
