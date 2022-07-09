a01f254b210e4d922e7a4b589be72f3f63aefba7
Jaroslaw Charchula <charchula@gmail.com>
2022-07-09 16:03:30 +0200

    add filter data file

---
+++ src/utils/filterData.js
@@ -0,0 +1,50 @@
+export const filterData = [
+    {
+      items: [
+        { name: 'Easy', value: 'easy' },
+        { name: 'Medium', value: 'medium' },
+        { name: 'Hard', value: 'hard' },
+      ],
+      placeholder: 'Difficulty',
+      queryName: 'difficulty',
+    },
+    {
+      items: [
+        { name: 'General Knowlage', value: '9' },
+        { name: 'Books', value: '10' },
+        { name: 'Films', value: '11' },
+        { name: 'Music', value: '12' },
+        { name: 'Musicals & Theaters', value: '13' },
+        { name: 'Television', value: '14' },
+        { name: 'Video Games', value: '15' },
+        { name: 'Board Games', value: '16' },
+        { name: 'Science & Nature', value: '17' },
+        { name: 'Science: Computers', value: '18' },
+        { name: 'Science: Mathematics', value: '19' },
+        { name: 'Mythology', value: '20' },
+        { name: 'Sports', value: '21' },
+        { name: 'Geography', value: '22' },
+        { name: 'History', value: '23' },
+        { name: 'Politics', value: '24' },
+        { name: 'Art', value: '25' },
+        { name: 'Celebrities', value: '26' },
+        { name: 'Animals', value: '27' },
+        { name: 'Vehicles', value: '28' },
+        { name: 'Comics', value: '29' },
+        { name: 'Science: Gadgets', value: '30' },
+        { name: 'Japanes Anime & Manga', value: '31' },
+        { name: 'Cartoon & Animation', value: '30' },
+        
+      ],
+      placeholder: 'Category',
+      queryName: 'category',
+    },
+    {
+      items: [
+        { name: 'Multiple Choice', value: 'multiple' },
+        { name: 'Ture / False', value: 'boolean' },
+      ],
+      placeholder: 'Type of answers',
+      queryName: 'type',
+    },
+];
