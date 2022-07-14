452e38199adc2409f8083cdd9336b3c1cfcbfe36
Jaroslaw Charchula <charchula@gmail.com>
2022-07-14 19:11:34 +0200

    add animation

--- src/components/StartingPage.js
+++ src/components/StartingPage.js
@@ -42,22 +42,63 @@ const StartingPage = ( { initialDataSetter } ) => {
   return (
     <main className='startingPage__container'>            
       <div className='startingPage__animationContainer'>              
-        <motion.div className='startingPage__okCloud'>   
-          <FaCheck />      
+        <motion.div className='startingPage__okCloud'
+          animate={{
+            y: [0, -20, 0],
+            x: [0 , -15, 0]
+          }}  
+          transition={{
+            repeat: Infinity,
+            duration: 4
+          }}
+          initial={{
+            rotate: -15
+           }}
+        >   
+          <FaCheck size='30px' />      
         </motion.div>
-        <motion.div className='startingPage__questionMarkCloud'>
-          <FaQuestion />
+        <motion.div className='startingPage__questionMarkCloud'
+           animate={{
+             y: [-10, -30, -10]
+            }}
+            transition={{
+             repeat: Infinity,
+             duration: 4,
+             delay: 0.5
+           }}
+           initial={{
+            y: -10
+           }}
+        >
+          <FaQuestion size='30px'/>
         </motion.div>
-        <motion.div className='startingPage__exclamationMarkCloud'>
-          <FaExclamation />
+        <motion.div className='startingPage__exclamationMarkCloud'
+          animate={{
+            y: [0, -20, 0],
+            x: [0, 15, 0]
+          }}  
+          transition={{
+            repeat: Infinity,
+            duration: 4,
+            delay: 0.75
+          }}
+          initial={{
+            rotate: 15
+           }}          
+        >
+          <FaExclamation size='30px'/>
         </motion.div>
       </div>
-      <h1>
+      <motion.h1
+        animate={{scale: 1}}
+        initial={{scale: 0}} 
+        transition={{duration: 1.5}}       
+      >
         <span className='startingPage__letterGreen'>Q</span>
         <span className='startingPage__letterRed'>U</span>
         <span className='startingPage__letterGreen'>I</span>
         <span className='startingPage__letterRed'>Z</span>
-      </h1> 
+      </motion.h1> 
       <div>
         <form onSubmit={handleSubmit}>
           <label htmlFor='name'>Type nickname:</label>
